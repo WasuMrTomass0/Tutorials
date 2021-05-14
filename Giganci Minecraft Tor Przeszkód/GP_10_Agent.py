2c7f2db507f40d3b4eb30660f844bd13bdd22e07
WasuMrTomass0 <39247298+WasuMrTomass0@users.noreply.github.com>
2021-05-14 20:02:02 +0200

    Update GP_10_Agent.py

--- Giganci Minecraft Tor Przeszkód/GP_10_Agent.py
+++ Giganci Minecraft Tor Przeszkód/GP_10_Agent.py
@@ -5,6 +5,16 @@ slot 1 - Tory
 slot 2 - Zasilane tory
 slot 3 - Stone (blok kamienia)
 slot 4 - Redstone
+
+kierunek:
+numer 1 - kierunek północ     (North)
+numer 2 - kierunek południe   (South)
+numer 3 - kierunek wschód     (East)
+numer 4 - kierunek zachód     (West)
+
+rodzaj torów:
+numer 1 - zwykłe tory
+numer 2 - zasilane tory
 """
 # Funkcja odpowiedzialna za przekazanie wyposażenia
 # naszemu agentowi
@@ -22,7 +32,42 @@ def uzupelnijBloki():
 
 
 # Funkcja odpowiedzialna za budowanie kolejki (za pomoca agenta)
-def budowanieKolejki(iloscTorow):
+def budowanieKolejki(iloscTorow, kierunek, rodzaj_torow):
+    # # # # Odczytanie argumentow funkcji # # # # 
+    
+    # Odczytanie kierunku i utworzenie nowej zmiennej pomocniczej
+    if kierunek == 1:
+        kierunekAgenta = CompassDirection.NORTH
+        pass
+    elif kierunek == 2:
+        kierunekAgenta = CompassDirection.SOUTH
+        pass
+    elif kierunek == 3:
+        kierunekAgenta = CompassDirection.EAST
+        pass
+    elif kierunek == 4:
+        kierunekAgenta = CompassDirection.WEST
+        pass
+    else:
+        # Wprowadzono niepoprawny kierunek. Wyświetl komunikat o błędzie i NIE TWÓRZ TORÓW
+        player.say("Podany niepoprawny kierunek! Spróbuj jeszcze raz.")
+        return
+        pass  # Koniec else
+
+    # Odczytanie rodzaju torow
+    if rodzaj_torow == 1:
+        slotTorow = 1
+        slotBlokuPodTorami = 3
+        pass
+    elif rodzaj_torow == 2:
+        slotTorow = 2
+        slotBlokuPodTorami = 4
+        pass
+    else:
+        # Wprowadzono niepoprawny rodzaj torów. Wyświetl komunikat o błędzie i NIE TWÓRZ TORÓW
+        player.say("Podano niepoprawny rodzaj torów! Spróbuj jeszcze raz.")
+        return
+        pass  # Koniec else
 
     # Utworzenie zmiennej
     # Liczba torów, które zostały już zbudowane
@@ -35,7 +80,7 @@ def budowanieKolejki(iloscTorow):
     # "Teleportacja" agenta
     # Pozycja agenta nie zmienia się. 
     # Zmieniamy jedynie jego kierunek - na południe ("patrzy")
-    agent.teleport(agent.get_position(), SOUTH)
+    agent.teleport(agent.get_position(), kierunekAgenta)
 
     # # Ustawienia pracy agenta # #
     # Stawianie bloków przy każdym ruchu
@@ -56,7 +101,7 @@ def budowanieKolejki(iloscTorow):
         elif agent.inspect(AgentInspection.BLOCK, DOWN) == 0:
             # Ustawienie slotu agenta na trójkę (slot kamienia)
             # Z tego slotu będą "zabierane" bloczki, które będziemy stawiać
-            agent.set_slot(3)
+            agent.set_slot(slotBlokuPodTorami)
             # Postawienie bloku pod agentem
             agent.place(DOWN)
             pass  # Koniec elif
@@ -65,7 +110,7 @@ def budowanieKolejki(iloscTorow):
         # to oznacza, że miejsce jest gotowe, aby dodać tory
         else:
             # Ustawienie slotu zawierającego tory
-            agent.set_slot(1)
+            agent.set_slot(slotTorow)
             # Przesunięcie agenta (równoważne z postawieniem bloku 
             #   - patrz instrukcja "agent.set_assist(PLACE_ON_MOVE, True)")
             agent.move(FORWARD, 1)
